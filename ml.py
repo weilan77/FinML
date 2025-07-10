d9dacd6419de45de96b9bffc80bbdec175aebb5a
weilan77 <gracetian1980@hotmail.com>
2025-07-10 15:18:26 +0800

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,1 +1,18 @@
 import pandas as pd
+import datetime
+import numpy as np
+import talib as ta
+import matplotlib.pyplot as plt
+from numpy import log, polyfit, sqrt, std, subtract
+from statsmodels.tsa.stattools import adfuller
+
+
+def rolling_adf(series, window=100):
+    def adf_calc(x):
+        result = adfuller(x)
+        return result[0]  # 返回ADF统计量
+
+    return series.rolling(window).apply(adf_calc, raw=True)
+
+
+
